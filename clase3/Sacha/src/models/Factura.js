706d2e24aa72c02d8f4b73a44bb3488f2a816554
sacha-buengueroff <sachabuengue@gmail.com>
2022-08-24 19:48:00 -0300

    segundo commit

---
+++ clase3/Sacha/src/models/Factura.js
@@ -0,0 +1,14 @@
+class Factura {
+
+    constructor(codigo, importe, items) {
+        this.codigo = codigo;
+        this.importe = importe;
+        this.items = items;
+    }
+
+    calcular() {
+        return 0;
+    }
+}
+
+export default Factura
